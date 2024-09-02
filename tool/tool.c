67b6e7319189bd7db983ff4bf58434a89707d21f
AyaSanae <2294760573@qq.com>
2024-09-02 22:52:50 +0800

    tool:frame_copy_dma():The variable dma_chan should first be assigned to chan

--- tool/tool.c
+++ tool/tool.c
@@ -4,10 +4,11 @@
 
 
 int frame_copy_dma(uint8_t *d_frame,uint8_t *s_frame,int dma_chan){
-    int chan;
+    int chan = dma_chan;
 
-    if(dma_chan == 100)
+    if(chan == 100){
        chan = dma_claim_unused_channel(1);
+    }
     dma_channel_config c = dma_channel_get_default_config(chan);
     channel_config_set_transfer_data_size(&c,DMA_SIZE_8);
     channel_config_set_read_increment(&c,true);
