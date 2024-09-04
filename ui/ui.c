ce73b43083c05c1a8e2b1c4bc977b8ddfd77fb64
AyaSanae <2294760573@qq.com>
2024-09-04 23:19:04 +0800

    ui: use struct FreqChangeCounter to to simplify the code

--- ui/ui.c
+++ ui/ui.c
@@ -137,39 +137,52 @@ static inline void DynamicRendering_INIT(usage_change uc,freq_change fc,tmp_chan
           ram_usage_incr_counter = 0;
           vram_usage_incr_counter = 0;
           cpu_tmp_incr_counter    = 0;
-          gpu_tmp_incr_counter  = 0;
+          gpu_tmp_incr_counter    = 0;
 
-          ram_use_incr_margin_counter = 0;
-          cpu_freq_incr_margin_counter = 0;
-          gpu_freq_incr_margin_counter = 0;
-          vram_use_incr_margin_counter = 0;
+          //FreqChangeCounter INIT
+          ram_use_FCC.change_margin_limiter = 0;
+          cpu_freq_FCC.change_margin_limiter = 0;
+          gpu_freq_FCC.change_margin_limiter = 0;
+          vram_use_FCC.change_margin_limiter = 0;
 
           //CPU FREQ INCREASE INIT
-          cpu_freq_incr_total = 0;
-          cpu_freq_after_incr = resce[RESC_PRE].cpu.freq_cur;
-          cpu_freq_incr_each = fc.cpu_freq_change / 100;
-          cpu_freq_incr_margin = fc.cpu_freq_change % 100;
+          cpu_freq_FCC.change_total  = 0;
+          cpu_freq_FCC.after_change  = resce[RESC_PRE].cpu.freq_cur;
+          cpu_freq_FCC.change_each   = fc.cpu_freq_change / 100;
+          cpu_freq_FCC.change_margin = fc.cpu_freq_change % 100;
+
 
           //GPU FREQ INCREASE INIT
-          gpu_freq_incr_total = 0;
-          gpu_freq_after_incr = resce[RESC_PRE].gpu.freq_cur;
-          gpu_freq_incr_each = fc.gpu_freq_change / 100;
-          gpu_freq_incr_margin = fc.gpu_freq_change % 100;
+          gpu_freq_FCC.change_total  = 0;
+          gpu_freq_FCC.after_change  = resce[RESC_PRE].gpu.freq_cur;
+          gpu_freq_FCC.change_each   = fc.gpu_freq_change / 100;
+          gpu_freq_FCC.change_margin = fc.gpu_freq_change % 100;
 
           //VRAM USE INCREASE INIT
-          vram_use_incr_total = 0;
-          vram_use_after_incr = (int_fast16_t)(resce[RESC_PRE].gpu.vram_cur * 10);
-          vram_use_incr_each = ((int_fast16_t)(fc.vram_change * 10)) / 100;
-          vram_use_incr_margin = ((int_fast16_t)(fc.vram_change * 10)) % 100;
+          vram_use_FCC.change_total  = 0;
+          vram_use_FCC.after_change  = (int_fast16_t)(resce[RESC_PRE].gpu.vram_cur * 10);
+          vram_use_FCC.change_each   = ((int_fast16_t)(fc.vram_change * 10)) / 100;
+          vram_use_FCC.change_margin = ((int_fast16_t)(fc.vram_change * 10)) % 100;
 
           //RAM USE INCREASE INIT
-          ram_use_incr_total = 0;
-          ram_use_after_incr = (int_fast16_t)(resce[RESC_PRE].ram.ram_cur * 10);
-          ram_use_incr_each = ((int_fast16_t)(fc.ram_change * 10)) / 100;
-          ram_use_incr_margin = ((int_fast16_t)(fc.ram_change * 10)) % 100;
+          ram_use_FCC.change_total  = 0;
+          ram_use_FCC.after_change  = (int_fast16_t)(resce[RESC_PRE].ram.ram_cur * 10);
+          ram_use_FCC.change_each   = ((int_fast16_t)(fc.ram_change * 10)) / 100;
+          ram_use_FCC.change_margin = ((int_fast16_t)(fc.ram_change * 10)) % 100;
 
 }
 
+static inline void Cal_frqe_change_each_time(FreqChangeCounter *FCC,DecimalNum_Components *DNC,float freq_change){
+    int_fast8_t sign = freq_change >= 0 ? 1 : -1;
+
+    FCC->after_change += FCC->change_each;
+    FCC->change_total = FCC->after_change + FCC->change_margin_limiter * sign;
+
+    DNC->ones_place     = FCC->change_total % 10; 
+    DNC->tens_place     = FCC->change_total / 10 % 10; 
+    DNC->hundre_place   = FCC->change_total / 100 % 10; 
+    DNC->thousand_place = FCC->change_total / 1000;
+}
 
 static void DynamicRendering(uint8_t *frame,uint8_t render_frameNum,resource *resce,
                                         int dma_chan,tmp_change tc,freq_change fc,usage_change uc){
@@ -181,47 +194,18 @@ static void DynamicRendering(uint8_t *frame,uint8_t render_frameNum,resource *re
 
         //like ((uc.cpu_usage_change >= 0) ? 1 : -1) 
         //it use to get the change sign
-
         cpu_usage_progress  = resce[RESC_PRE].cpu.core_usage + cpu_usage_incr_counter * ((uc.cpu_usage_change >= 0) ? 1 : -1);
         gpu_usage_progress  = resce[RESC_PRE].gpu.core_usage + gpu_usage_incr_counter  * ((uc.gpu_usage_change >= 0) ? 1 : -1);
         ram_usage_progress  = resce[RESC_PRE].ram.ram_usage + ram_usage_incr_counter * ((uc.ram_usage_change >= 0) ? 1 : -1);
-
         vram_usage_progress = resce[RESC_PRE].gpu.vram_usage + vram_usage_incr_counter * ((uc.vram_usage_change >= 0) ? 1 : -1);
         cpu_tmp_progress    = resce[RESC_PRE].cpu.tmp + cpu_tmp_incr_counter * ((tc.cpu_tmp_change >= 0) ? 1 : -1);
         gpu_tmp_progress    = resce[RESC_PRE].gpu.tmp + gpu_tmp_incr_counter * ((tc.gpu_tmp_change >= 0) ? 1 : -1);
 
-        //CPU FREQ INCREASE CAL
-        cpu_freq_after_incr += cpu_freq_incr_each;
-        cpu_freq_incr_total =  cpu_freq_after_incr + cpu_freq_incr_margin_counter * ((fc.cpu_freq_change >= 0) ? 1 : -1);
-        //GET CPU FREQ EACH NUM;
-        cpu_freq_DNC.ones_place     = cpu_freq_incr_total % 10; 
-        cpu_freq_DNC.tens_place     = cpu_freq_incr_total / 10 % 10; 
-        cpu_freq_DNC.hundre_place   = cpu_freq_incr_total / 100 % 10; 
-        cpu_freq_DNC.thousand_place = cpu_freq_incr_total / 1000; 
-
-        //GPU FREQ INCREASE CAL
-        gpu_freq_after_incr += gpu_freq_incr_each;
-        gpu_freq_incr_total =  gpu_freq_after_incr + gpu_freq_incr_margin_counter * ((fc.gpu_freq_change >= 0) ? 1 : -1);
-        //GET GlU FREQ EACH NUM;
-        gpu_freq_DNC.ones_place   = gpu_freq_incr_total % 10; 
-        gpu_freq_DNC.tens_place   = gpu_freq_incr_total / 10 % 10; 
-        gpu_freq_DNC.hundre_place = gpu_freq_incr_total / 100 % 10; 
-        gpu_freq_DNC.thousand_place = gpu_freq_incr_total / 1000; 
-
-        //RAM USE INCREASE CAL
-        ram_use_after_incr  += ram_use_incr_each;
-        ram_use_incr_total   = ram_use_after_incr + ram_use_incr_margin_counter * ((fc.ram_change >= 0)? 1 : -1);
-        ram_use_DNC.ones_place   = ram_use_incr_total % 10; 
-        ram_use_DNC.tens_place   = ram_use_incr_total / 10 % 10; 
-        ram_use_DNC.hundre_place = ram_use_incr_total / 100 % 10;
-
-        //VRAM USE INCREASE CAL
-        vram_use_after_incr  += vram_use_incr_each;
-        vram_use_incr_total   = vram_use_after_incr + vram_use_incr_margin_counter * ((fc.vram_change >= 0)? 1 : -1);
-        vram_use_DNC.ones_place   = vram_use_incr_total % 10; 
-        vram_use_DNC.tens_place   = vram_use_incr_total / 10 % 10; 
-        vram_use_DNC.hundre_place = vram_use_incr_total / 100 % 10; 
-
+        Cal_frqe_change_each_time(&cpu_freq_FCC,&cpu_freq_DNC,fc.cpu_freq_change);
+        Cal_frqe_change_each_time(&gpu_freq_FCC,&gpu_freq_DNC,fc.gpu_freq_change);
+        Cal_frqe_change_each_time(&ram_use_FCC,&ram_use_DNC,fc.ram_change);
+        Cal_frqe_change_each_time(&vram_use_FCC,&vram_use_DNC,fc.vram_change);
+        
 
         checkAndIncrement(&cpu_usage_incr_counter, uc.cpu_usage_change);
         checkAndIncrement(&gpu_usage_incr_counter, uc.gpu_usage_change);
@@ -229,10 +213,11 @@ static void DynamicRendering(uint8_t *frame,uint8_t render_frameNum,resource *re
         checkAndIncrement(&ram_usage_incr_counter, uc.ram_usage_change);
         checkAndIncrement(&cpu_tmp_incr_counter, tc.cpu_tmp_change);
         checkAndIncrement(&gpu_tmp_incr_counter , tc.gpu_tmp_change);
-        checkAndIncrement(&cpu_freq_incr_margin_counter, cpu_freq_incr_margin);
-        checkAndIncrement(&gpu_freq_incr_margin_counter, gpu_freq_incr_margin);
-        checkAndIncrement(&vram_use_incr_margin_counter, vram_use_incr_margin);
-        checkAndIncrement(&ram_use_incr_margin_counter,  ram_use_incr_margin);
+
+        checkAndIncrement(&ram_use_FCC.change_margin_limiter, ram_use_FCC.change_margin);
+        checkAndIncrement(&cpu_freq_FCC.change_margin_limiter, cpu_freq_FCC.change_margin);
+        checkAndIncrement(&gpu_freq_FCC.change_margin_limiter, gpu_freq_FCC.change_margin);
+        checkAndIncrement(&vram_use_FCC.change_margin_limiter, vram_use_FCC.change_margin);
 
         dma_channel_wait_for_finish_blocking(dma_chan);
 
@@ -249,14 +234,14 @@ static void DynamicRendering(uint8_t *frame,uint8_t render_frameNum,resource *re
         OLED_WriteChar_fix(frame,48,40,gpu_freq_DNC.thousand_place + '0');
 
         //RAM USE
-        OLED_WriteChar_fix(frame,49,56,ram_use_DNC.ones_place   + '0');
+        OLED_WriteChar_fix(frame,66,56,ram_use_DNC.ones_place + '0');
         OLED_WriteChar_fix(frame,57,56,ram_use_DNC.tens_place   + '0');
-        OLED_WriteChar_fix(frame,66,56,ram_use_DNC.hundre_place + '0');
+        OLED_WriteChar_fix(frame,49,56,ram_use_DNC.hundre_place   + '0');
 
         //VRAM USE
-        OLED_WriteChar_fix(frame,49,48,vram_use_DNC.ones_place   + '0');
+        OLED_WriteChar_fix(frame,66,48,vram_use_DNC.ones_place + '0');
         OLED_WriteChar_fix(frame,57,48,vram_use_DNC.tens_place   + '0');
-        OLED_WriteChar_fix(frame,66,48,vram_use_DNC.hundre_place + '0');
+        OLED_WriteChar_fix(frame,49,48,vram_use_DNC.hundre_place   + '0');
 
         // Draw RAM,VRAM USE DECIMAL POINT
         Draw_decimal_point(frame);
--- ui/ui.h
+++ ui/ui.h
@@ -13,6 +13,14 @@ typedef struct
    int_fast16_t thousand_place;
 }DecimalNum_Components;
 
+typedef struct{
+    int_fast16_t change_each;
+    int_fast16_t change_margin;
+    int_fast16_t change_margin_limiter;
+    int_fast16_t after_change;
+    int_fast16_t change_total;
+}FreqChangeCounter;    //Smooth transitions for displaying various values
+
 //counter--Smooth transitions for displaying various values.
 static int_fast16_t cpu_usage_incr_counter;   
 static int_fast16_t gpu_usage_incr_counter;
@@ -22,41 +30,21 @@ static int_fast16_t cpu_tmp_incr_counter; // CPU temperature
 static int_fast16_t gpu_tmp_incr_counter; // GPU temperature
 
 //FOR CPU FREQ
-static int_fast16_t cpu_freq_incr_each;
-static int_fast16_t cpu_freq_incr_margin; 
-static int_fast16_t cpu_freq_after_incr;
-static int_fast16_t cpu_freq_after_incr_margin;
-static int_fast16_t cpu_freq_incr_total;
-static int_fast16_t cpu_freq_incr_margin_counter;  //each increse 1 to deal with margin
+static FreqChangeCounter cpu_freq_FCC;
 static DecimalNum_Components cpu_freq_DNC;
 
 
 //FOR GPU FREQ
-static int_fast16_t gpu_freq_incr_each;
-static int_fast16_t gpu_freq_incr_margin; 
-static int_fast16_t gpu_freq_after_incr;
-static int_fast16_t gpu_freq_after_incr_margin;
-static int_fast16_t gpu_freq_incr_total;
-static int_fast16_t gpu_freq_incr_margin_counter;  
+static FreqChangeCounter gpu_freq_FCC;
 static DecimalNum_Components gpu_freq_DNC;
 
 
 //FOR VRAM USE
-static int_fast16_t vram_use_incr_each;
-static int_fast16_t vram_use_incr_margin; 
-static int_fast16_t vram_use_after_incr;
-static int_fast16_t vram_use_after_incr_margin;
-static int_fast16_t vram_use_incr_total;
-static int_fast16_t vram_use_incr_margin_counter;
+static FreqChangeCounter vram_use_FCC;
 static DecimalNum_Components vram_use_DNC;
 
 //FOR RAM USE
-static int_fast16_t ram_use_incr_each;
-static int_fast16_t ram_use_incr_margin; 
-static int_fast16_t ram_use_after_incr;
-static int_fast16_t ram_use_after_incr_margin;
-static int_fast16_t ram_use_incr_total;
-static int_fast16_t ram_use_incr_margin_counter;
+static FreqChangeCounter ram_use_FCC;
 static DecimalNum_Components ram_use_DNC;
 
 static int_fast16_t cpu_usage_progress;
@@ -83,6 +71,7 @@ static inline tmp_change   Cal_resTmpChange(resource *resce);
 static inline freq_change  Cal_resFreqChange(resource *resce);
 static inline usage_change Cal_resUsageChange(resource *resce);
 
+static inline void         Cal_frqe_change_each_time(FreqChangeCounter *FCC,DecimalNum_Components *DNC,float freq_change);
 
 void Render_ResPage(uint8_t *frame,resource *resce);
 
